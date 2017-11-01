a962b1636d8203e4dc3a317476c14cd4f9a3fd61
Mahammad Valiyev <mehemmedv@gmail.com>
2017-11-01 17:36:41 +0100

    mixed workload added to scripts

--- scripts/test_runtime.sh
+++ scripts/test_runtime.sh
@@ -4,16 +4,48 @@ for i in 100 1000 10000 100000 100000
 do
    for j in 10 20 30 40
    do
-      ./DB_Generator $i $(($i * $j / 100)) 1000000
+      ./DB_Generator $i $(($i * $j / 100)) 1000000 # Generate a graph
+      
+      # Test DFS for unsorted graph
       ./DB_Benchmark -CSR DFS unsorted 200 20 >> result.txt
+      ./DB_Benchmark -AL DFS unsorted 200 20 >> result.txt
+      ./DB_Benchmark -ALV2 DFS unsorted 200 20 >> result.txt
+      
+      # Test BFS for unsorted graph
+      ./DB_Benchmark -CSR BFS unsorted 200 20 >> result.txt
       ./DB_Benchmark -AL BFS unsorted 200 20 >> result.txt
+      ./DB_Benchmark -ALV2 BFS unsorted 200 20 >> result.txt
+      
+      # Test Dijkstra for unsorted graph
+      ./DB_Benchmark -CSR Dijkstra unsorted 200 20 >> result.txt
+      ./DB_Benchmark -AL Dijkstra unsorted 200 20 >> result.txt
       ./DB_Benchmark -ALV2 Dijkstra unsorted 200 20 >> result.txt
+      
+      # Test DFS for sorted graph
       ./DB_Benchmark -CSR DFS sorted 200 20 >> result.txt
+      ./DB_Benchmark -AL DFS sorted 200 20 >> result.txt
+      ./DB_Benchmark -ALV2 DFS sorted 200 20 >> result.txt
+      
+      # Test BFS for sorted graph
+      ./DB_Benchmark -CSR BFS sorted 200 20 >> result.txt
       ./DB_Benchmark -AL BFS sorted 200 20 >> result.txt
+      ./DB_Benchmark -ALV2 BFS sorted 200 20 >> result.txt
+
+      # Test Dijkstra for sorted graph
+      ./DB_Benchmark -CSR Dijkstra sorted 200 20 >> result.txt
+      ./DB_Benchmark -AL Dijkstra sorted 200 20 >> result.txt
       ./DB_Benchmark -ALV2 Dijkstra sorted 200 20 >> result.txt
+      
+      # Test DFS for mixed workload
       ./DB_Benchmark -CSR DFS mixed 200 4 >> result.txt
+      ./DB_Benchmark -AL DFS mixed 200 4 >> result.txt
+      ./DB_Benchmark -ALV2 DFS mixed 200 4 >> result.txt
+      
+      # Test BFS for mixed workload
+      ./DB_Benchmark -CSR BFS mixed 200 4 >> result.txt
       ./DB_Benchmark -AL BFS mixed 200 4 >> result.txt
-      ./DB_Benchmark -ALV2 Dijkstra mixed 200 4 >> result.txt
+      ./DB_Benchmark -ALV2 BFS mixed 200 4 >> result.txt
+      
       str="out"$i"_"$(($i*$j/100))".txt"
       mv out.txt $str
    done
