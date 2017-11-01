a84055eb9eec1a89ded8bbb412a70838b3f5b1c6
Mahammad Valiyev <mehemmedv@gmail.com>
2017-11-01 17:11:07 +0100

    script modification

--- scripts/test_runtime.sh
+++ scripts/test_runtime.sh
@@ -8,9 +8,15 @@ do
    for j in 10 20 30 40
    do
       ./DB_Generator $i $(($i * $j / 100)) 1000000
-      ./DB_Benchmark -CSR BFS unsorted 200 20 >> result.txt
+      ./DB_Benchmark -CSR DFS unsorted 200 20 >> result.txt
       ./DB_Benchmark -AL BFS unsorted 200 20 >> result.txt
-      ./DB_Benchmark -ALV2 BFS unsorted 200 20 >> result.txt
+      ./DB_Benchmark -ALV2 Dijkstra unsorted 200 20 >> result.txt
+      ./DB_Benchmark -CSR DFS sorted 200 20 >> result.txt
+      ./DB_Benchmark -AL BFS sorted 200 20 >> result.txt
+      ./DB_Benchmark -ALV2 Dijkstra sorted 200 20 >> result.txt
+      ./DB_Benchmark -CSR DFS sorted 200 5 >> result.txt
+      ./DB_Benchmark -AL BFS sorted 200 5 >> result.txt
+      ./DB_Benchmark -ALV2 Dijkstra sorted 200 5 >> result.txt
       str="out"$i"_"$(($i*$j/100))".txt"
       sudo mv out.txt $str
    done
