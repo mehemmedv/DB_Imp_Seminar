18e0c1a3b7e4f3d0b332811a03d0d2026d5b1e1c
Mahammad Valiyev <mehemmedv@gmail.com>
2017-12-05 12:58:29 +0100

    CSR second version implemented with having more than enough memory for edges

---
+++ include/CSRGraphV2.h
@@ -0,0 +1,56 @@
+//
+// Created by mahammad on 12/5/17.
+//
+
+#ifndef DB_IMP_SEMINAR_CSRGRAPHV2_H
+#define DB_IMP_SEMINAR_CSRGRAPHV2_H
+
+
+#include <vector>
+#include <iostream>
+
+// 'Compressed sparse row' implementation of Graph
+class CSRGraphV2 {
+    int *edges, *offsets;
+    int* weights;
+    uint64_t cur_idx, v, e;
+    uint64_t current_size = 0;
+
+public:
+    CSRGraphV2(uint64_t v, uint64_t e) : v(v), e(e) {
+        current_size = e + 10;
+        edges = new int[current_size];
+        offsets = new int[v + 2];
+        cur_idx = 0;
+        weights = new int[current_size];
+    }
+
+    ~CSRGraphV2(){
+        delete[] edges;
+        delete[] offsets;
+        delete[] weights;
+        //std::cout<<"CSRGraph delete"<<std::endl;
+    }
+
+    void add_edge(int from, std::vector<int> &to, std::vector<int>& w);
+
+    void add_edge(int from, int to, int weight = 0);
+
+    void finished();
+
+    void sortByEdgesByNodeId();
+
+    inline int* begin(int cur_vertex){
+        return &(edges[offsets[cur_vertex]]);
+    }
+
+    inline int* end(int cur_vertex){
+        return &(edges[offsets[cur_vertex + 1]]);
+    }
+
+    inline int* begin_weights(int cur_vertex){
+        return &(weights[offsets[cur_vertex]]);
+    }
+};
+
+#endif //DB_IMP_SEMINAR_CSRGRAPHV2_H
---
+++ src/CSRGraphV2.cpp
@@ -0,0 +1,65 @@
+//
+// Created by mahammad on 12/5/17.
+//
+
+
+#include "../include/CSRGraphV2.h"
+#include <string.h>
+#include <iostream>
+#include <algorithm>
+#include <stdint-gcc.h>
+
+void CSRGraphV2::add_edge(int from, std::vector<int>& to, std::vector<int>& w) {
+    offsets[from] = cur_idx;
+    uint64_t temp_cur_idx = cur_idx;
+    for(int i : to){
+        edges[cur_idx++] = i;
+    }
+    for(int i : w){
+        weights[temp_cur_idx++] = i;
+    }
+}
+
+void CSRGraphV2::finished() {
+    offsets[v+1] = e;
+}
+
+void CSRGraphV2::add_edge(int from, int to, int weight) {
+    // adding a new edge
+    bool isFull = false;
+    int *dest_edges = edges, *dest_weights = weights;
+
+    if(e == current_size){
+        isFull = true;
+        current_size += 10;
+    }
+    if(!isFull) {
+        memmove(dest_edges + offsets[from + 1] + 1, edges + offsets[from + 1], sizeof(int) * (e - offsets[from + 1]));
+        dest_edges[offsets[from + 1]] = to;
+        memmove(dest_weights + offsets[from + 1] + 1, weights + offsets[from + 1], sizeof(int) * (e - offsets[from + 1]));
+        dest_weights[offsets[from + 1]] = weight;
+    } else {
+        dest_edges = new int[current_size];
+        memcpy(dest_edges, edges, sizeof(int) * offsets[from + 1]);
+        dest_edges[offsets[from + 1]] = to;
+        memcpy(dest_edges + offsets[from + 1] + 1, edges + offsets[from + 1], sizeof(int) * (e - offsets[from + 1]));
+        delete[] edges;
+        edges = dest_edges;
+        dest_weights = new int[current_size];
+        memcpy(dest_weights, weights, sizeof(int) * offsets[from + 1]);
+        dest_weights[offsets[from + 1]] = weight;
+        memcpy(dest_weights + offsets[from + 1] + 1, weights + offsets[from + 1], sizeof(int) * (e - offsets[from + 1]));
+        delete[] weights;
+        weights = dest_weights;
+    }
+
+    ++e;
+    for(int i = from + 1; i <= v + 1; ++i) // offset for the vertices (from + 1 .. v + 1) is one index shifted
+        ++offsets[i];
+}
+
+void CSRGraphV2::sortByEdgesByNodeId() {
+    for(int i = 1; i <= v; ++i){
+        std::sort(edges + offsets[i], edges + offsets[i + 1]);
+    }
+}
