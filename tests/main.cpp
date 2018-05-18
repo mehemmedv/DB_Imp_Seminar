50e775256d109b0d036c24eb139dbd49a03f372b
Mahammad Valiyev <mehemmedv@gmail.com>
2018-05-19 01:03:15 +0200

    Benchmarking on get_neighbors function

--- tests/main.cpp
+++ tests/main.cpp
@@ -19,29 +19,119 @@ int main(int argc, char** argv) {
 
     srand(time(NULL));
 
-    ARTGraph* graph = new ARTGraph(5, 0);
-    //ALGraphV2* graph = new ALGraphV2(3456 + 1, 0);
-    for(int i = 1; i <= 15678; ++i)
-        for(uint32_t j = 1; j <= 15678; ++j){
-            //uint32_t to = ((rand() % 10007) * (rand() % 10007)) % 10000000;
-            uint32_t to = j;
-            //edges[i].push_back(to);
-            graph->add_edge(i, to, 100);
+    std::string type = argv[1];
+    if(type == "ART"){
+        ARTGraph* graph = new ARTGraph(15678 + 1, 0);
+
+        for(int i = 1; i <= 15678; ++i)
+            for(uint32_t j = 1; j <= 15678; ++j){
+                //uint32_t to = ((rand() % 10007) * (rand() % 10007)) % 10000000;
+                uint32_t to = j;
+                //edges[i].push_back(to);
+                graph->add_edge(i, to, 100);
+            }
+
+        auto total_update = 0LL;
+        auto begin_update = std::chrono::high_resolution_clock::now();
+        for(int i = 1; i <= 15678;++ i) {
+            //std::vector<uint32_t> res;
+            for(uint32_t j : graph->get_neighbors(i));
+                //res.push_back(j);
+            /*
+            std::sort(res.begin(), res.end());
+            std::sort(edges[i].begin(), edges[i].end());
+
+            if (res != edges[i])
+                std::cout << i << " : Different result " << std::endl;
+            */
         }
+        auto end_update = std::chrono::high_resolution_clock::now();
+        total_update += (std::chrono::duration_cast<std::chrono::milliseconds>(end_update - begin_update)).count();
+        std::cout << type << " Get Neighbors time: " << total_update << std::endl;
+    } else if(type == "CSRV2"){
+        CSRGraphV2* graph = new CSRGraphV2(15678 + 1, 0);
 
-    for(int i = 1; i <= 15678;++ i) {
-        //std::vector<uint32_t> res;
-        for(uint32_t j : graph->get_neighbors(i));
+        for(int i = 1; i <= 15678; ++i)
+            for(uint32_t j = 1; j <= 15678; ++j){
+                //uint32_t to = ((rand() % 10007) * (rand() % 10007)) % 10000000;
+                uint32_t to = j;
+                //edges[i].push_back(to);
+                graph->add_edge(i, to, 100);
+            }
+
+        auto total_update = 0LL;
+        auto begin_update = std::chrono::high_resolution_clock::now();
+        for(int i = 1; i <= 15678;++ i) {
+            //std::vector<uint32_t> res;
+            for(uint32_t j : graph->get_neighbors(i));
             //res.push_back(j);
+            /*
+            std::sort(res.begin(), res.end());
+            std::sort(edges[i].begin(), edges[i].end());
 
-        /*
-        std::sort(res.begin(), res.end());
-        std::sort(edges[i].begin(), edges[i].end());
+            if (res != edges[i])
+                std::cout << i << " : Different result " << std::endl;
+            */
+        }
+        auto end_update = std::chrono::high_resolution_clock::now();
+        total_update += (std::chrono::duration_cast<std::chrono::milliseconds>(end_update - begin_update)).count();
+        std::cout << type << " Get Neighbors time: " << total_update << std::endl;
+    } else if(type == "ALV2"){
+        ALGraphV2* graph = new ALGraphV2(15678 + 1, 0);
 
-        if (res != edges[i])
-            std::cout << i << " : Different result " << std::endl;
-        */
+        for(int i = 1; i <= 15678; ++i)
+            for(uint32_t j = 1; j <= 15678; ++j){
+                //uint32_t to = ((rand() % 10007) * (rand() % 10007)) % 10000000;
+                uint32_t to = j;
+                //edges[i].push_back(to);
+                graph->add_edge(i, to, 100);
+            }
+
+        auto total_update = 0LL;
+        auto begin_update = std::chrono::high_resolution_clock::now();
+        for(int i = 1; i <= 15678;++ i) {
+            //std::vector<uint32_t> res;
+            for(uint32_t j : graph->get_neighbors(i));
+            //res.push_back(j);
+            /*
+            std::sort(res.begin(), res.end());
+            std::sort(edges[i].begin(), edges[i].end());
+
+            if (res != edges[i])
+                std::cout << i << " : Different result " << std::endl;
+            */
+        }
+        auto end_update = std::chrono::high_resolution_clock::now();
+        total_update += (std::chrono::duration_cast<std::chrono::milliseconds>(end_update - begin_update)).count();
+        std::cout << type << " Get Neighbors time: " << total_update << std::endl;
+    } else if(type == "AL"){
+        ALGraph* graph = new ALGraph(15678 + 1, 0);
+
+        for(int i = 1; i <= 15678; ++i)
+            for(uint32_t j = 1; j <= 15678; ++j){
+                //uint32_t to = ((rand() % 10007) * (rand() % 10007)) % 10000000;
+                uint32_t to = j;
+                //edges[i].push_back(to);
+                graph->add_edge(i, to, 100);
+            }
+
+        auto total_update = 0LL;
+        auto begin_update = std::chrono::high_resolution_clock::now();
+        for(int i = 1; i <= 15678;++ i) {
+            //std::vector<uint32_t> res;
+            for(uint32_t j : graph->get_neighbors(i));
+            //res.push_back(j);
+            /*
+            std::sort(res.begin(), res.end());
+            std::sort(edges[i].begin(), edges[i].end());
+
+            if (res != edges[i])
+                std::cout << i << " : Different result " << std::endl;
+            */
+        }
+        auto end_update = std::chrono::high_resolution_clock::now();
+        total_update += (std::chrono::duration_cast<std::chrono::milliseconds>(end_update - begin_update)).count();
+        std::cout << type << " Get Neighbors time: " << total_update << std::endl;
     }
-
     return 0;
 }
